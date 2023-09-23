c24123163311b28b76123dd38bf0557f15e28e63
Марат Ашрапов <todgametwo@gmail.com>
2023-09-23 15:13:29 +0400

    Добавлен класс Product.

---
+++ FlowerShopResourceManagementSystem/ICRUD.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowerShopResourceManagementSystem
+{
+  internal interface ICRUD
+  {
+    void Create();
+
+    void Read();
+
+    void Update();
+
+    void Delete();
+  }
+}
---
+++ FlowerShopResourceManagementSystem/Product.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowerShopResourceManagementSystem
+{
+  internal class Product
+  {
+    #region Свойства
+    /// <summary>
+    /// Название товара.
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// Цена товара.
+    /// </summary>
+    public double Price { get; set; }
+
+    /// <summary>
+    /// Количество товара на складе.
+    /// </summary>
+    public int QuantityInStock { get; set; }
+
+    /// <summary>
+    /// Общее количество купленных товаров.
+    /// </summary>
+    public int TotalPurchasesCount {  get; set; }
+
+    /// <summary>
+    /// Общее количество проданных товаров.
+    /// </summary>
+    public int TotalSalesCount { get; set; }
+
+    #endregion
+
+    #region Конструктор
+    public Product(string name, double price, int quantity) 
+    {
+      Name = name;
+      Price = price;
+      Quantity = quantity;
+    }
+    #endregion
+  }
+}
